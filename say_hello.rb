017bc85285e891cbd96ca7a3ea86ba9b22ec06cf
Batsaikhan Ulambayar <u.batsaikhan@gmail.com>
2021-02-25 01:31:54 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,30 @@
-def say_hello (name = "Ruby Programmer")
-  puts "Hello #{name}!"
+class String
+  def is_integer?
+    self.to_i.to_s == self
+  end
+  
 end
 
-# Build your say_hello method here
+s = "abc5bc3"
+
+arr = s.split("")
+new_arr = []
+
+arr.each {|i| 
+  if i.is_integer?
+    i = i.to_i
+    loop do
+      new_arr << 1
+      i -=1
+      if i == 0
+        break
+      end
+    end
+  else
+    new_arr << i
+  end
+}
+
+puts new_arr
+
+
